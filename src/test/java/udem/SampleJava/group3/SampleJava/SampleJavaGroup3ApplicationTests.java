fc2053c883341e960f62f9b86e78197af69aedd8
JuanDelahozMiranda <juan.delahozdevelop@gmail.com>
2021-10-12 20:28:35 -0500

    set-up project

---
+++ src/test/java/udem/SampleJava/group3/SampleJava/SampleJavaGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package udem.SampleJava.group3.SampleJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJavaGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
